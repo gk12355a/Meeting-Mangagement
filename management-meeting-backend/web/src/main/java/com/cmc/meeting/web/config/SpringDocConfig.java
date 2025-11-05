1b3d7546392e889ae24be449fa228d9025c3624e
gk12355a <kiendotri@gmail.com>
2025-11-05 11:14:14 +0700

    add authentoken to swagger

--- management-meeting-backend/web/src/main/java/com/cmc/meeting/web/config/SpringDocConfig.java
+++ management-meeting-backend/web/src/main/java/com/cmc/meeting/web/config/SpringDocConfig.java
@@ -3,18 +3,32 @@ package com.cmc.meeting.web.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("Meeting Room Booking API")
+                        .title("Booking API")
                         .version("1.0.0")
                         .description("Hệ thống API quản lý và đặt lịch họp")
-                        .license(new License().name("CMC Global").url("https://cmcglobal.com.vn/")));
+                        .license(new License().name("CMC Global").url("https://cmcglobal.com.vn/")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
