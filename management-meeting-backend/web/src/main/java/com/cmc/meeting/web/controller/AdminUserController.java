a24bf90d31decb11a4724b2a2eef8b3d7dc0340c
Chu <chuduong2504@gmail.com>
2025-11-11 23:36:23 +0700

    update

--- management-meeting-backend/web/src/main/java/com/cmc/meeting/web/controller/AdminUserController.java
+++ management-meeting-backend/web/src/main/java/com/cmc/meeting/web/controller/AdminUserController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
